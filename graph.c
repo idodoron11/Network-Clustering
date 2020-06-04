b288225747c0d5c727ddc88384a281fa0b19193a
Ido Doron <ido.doron@gmail.com>
2020-06-04 18:30:11 +0300

    Added a new field to graph structure, to count how many edges are in G. Modified a few functions to support the change. Added a function to get the degree of a given vertex in G.

--- graph.c
+++ graph.c
@@ -3,9 +3,10 @@
 #include <assert.h>
 #include "graph.h"
 
-graph* constructGraphFromInput(int n, int* adjMat){
+graph* constructGraphFromInput(int n, int m, int* adjMat){
     graph* G = (graph*) malloc(sizeof(graph));
     G->n = n;
+    G->m  = m;
     G->adjMat = adjMat;
     return G;
 }
@@ -13,6 +14,7 @@ graph* constructGraphFromInput(int n, int* adjMat){
 graph* constructEmptyGraph(int n){
     graph* G = (graph*) malloc(sizeof(graph));
     G->n = n;
+    G->m = 0;
     G->adjMat = (int*) calloc(n * n, sizeof(int));
     return G;
 }
@@ -22,21 +24,26 @@ void destroyGraph(graph* G){
     free(G);
 }
 
-void setEdge(graph* G, int i, int j, int value){
+char setEdge(graph* G, int i, int j, int value){
     int n = G->n;
     assert(value == 0 || value == 1);
     assert(i>=0 && i<n);
     assert(j>=0 && j<n);
-    G->adjMat[i * n + j] = 1;
-    G->adjMat[j * n + i] = 1;
+    if(G->adjMat[i * n + j] == value)
+        return 0;
+    else {
+        G->adjMat[i * n + j] = value;
+        G->adjMat[j * n + i] = value;
+        return 1;
+    }
 }
 
 void attachVertices(graph* G, int i, int j){
-    setEdge(G, i, j, 1);
+    G->m += setEdge(G, i, j, 1);
 }
 
 void detachVertices(graph* G, int i, int j){
-    setEdge(G, i, j, 0);
+    G->m -= setEdge(G, i, j, 0);
 }
 
 int getEdge(graph* G, int i, int j){
@@ -50,4 +57,14 @@ void printGraph(graph* G){
         printf("%d", G->adjMat[i]);
         printf((i+1) % n == 0 ? "\n" : "\t");
     }
+}
+
+int getDegree(graph* G, int i){
+    int n = G->n;
+    int* neighborsVector = G->adjMat + (i*n);
+    int k;
+    int sum = 0;
+    for(k = 0; k < n; ++k)
+        sum += neighborsVector[k];
+    return sum;
 }
--- graph.h
+++ graph.h
@@ -5,21 +5,25 @@
 typedef struct _graph {
     /* Number of vertices.
      * V={1,2,...,n} */
-    int		n;
+    int	n;
 
     /* Adjacency matrix of size nXn
      * E={(i,j) s.t. adjMat[i*n + j]=1} */
     int* adjMat;
+
+    /* number of edges */
+    int m;
 } graph;
 
 /**
  * Creates a new graph object.
  * @param n number of vertices
+ * @param m number of edges
  * @param adjMat a n by n matrix consisting of zeros and ones only, represented by a n^2 long vector.
  *          Should be a symmetric matrix.
  * @return a reference to a new graph object, defined by the given adjacency matrix.
  */
-graph* constructGraphFromInput(int n, int* adjMat);
+graph* constructGraphFromInput(int n, int m, int* adjMat);
 
 /**
  * Creates a new graph with n vertices and no edges.
@@ -35,7 +39,7 @@ graph* constructEmptyGraph(int n);
 void destroyGraph(graph* G);
 
 /**
- * Creates a new edge (i,j) in the graph G.
+ * Creates a new edge (i,j) in the graph G, and updates G->m accordingly.
  * @param G a pointer to a graph.
  * @param i the vertex from which the edge exists. Should satisfy 0 <= i < G->n.
  * @param j the vertex to which the edge enters. Should satisfy 0 <= j < G->n.
@@ -43,12 +47,12 @@ void destroyGraph(graph* G);
 void attachVertices(graph* G, int i, int j);
 
 /**
- * Removes the edge (i,j) in the graph G.
+ * Removes the edge (i,j) in the graph G, and updates G->m accordingly.
  * @param G a pointer to a graph.
  * @param i the vertex from which the edge exists. Should satisfy 0 <= i < G->n.
  * @param j the vertex to which the edge enters. Should satisfy 0 <= j < G->n.
  */
-void attachVertices(graph* G, int i, int j);
+void detachVertices(graph* G, int i, int j);
 
 /**
  * Sets the value in the (i,j) and (j,i) entries of G's adjacency matrix.
@@ -56,8 +60,9 @@ void attachVertices(graph* G, int i, int j);
  * @param i the vertex from which the edge exists. Should satisfy 0 <= i < G->n.
  * @param j the vertex to which the edge enters. Should satisfy 0 <= j < G->n.
  * @param value should satisfy 0==value or 1==value
+ * @return 1 if the change was necessary and 0 otherwise.
  */
-void setEdge(graph* G, int i, int j, int value);
+char setEdge(graph* G, int i, int j, int value);
 
 /**
  * Returns the value kept in the (i,j) entry of G's adjacency matrix.
@@ -73,5 +78,12 @@ int getEdge(graph* G, int i, int j);
  * @param G a not too large graph.
  */
 void printGraph(graph* G);
+
+/**
+ * @param G a graph.
+ * @param i a vertex in G. should satisfy 0 <= i < G->n.
+ * @return The degree of vertex i, namely the number of undirected edges it's connected to.
+ */
+int getDegree(graph* G, int i);
 
 #endif /*GRAPH_H*/
