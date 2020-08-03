4b7a81abc49bd5f20bac26dcb264f73c38c91a09
Ido Doron <ido.doron@gmail.com>
2020-08-03 14:50:29 +0300

    Created LinkedList structure, and modified VerticesGroup to use it.

--- VerticesGroup.c
+++ VerticesGroup.c
@@ -6,39 +6,18 @@
 
 VerticesGroup *createVerticesGroup() {
     VerticesGroup *group = malloc(sizeof(VerticesGroup));
-    group->size = 0;
+    group->verticesList = createLinkedList();
     return group;
 }
 
 void freeVerticesGroup(VerticesGroup *group) {
-    VertexNode *temp;
-    VertexNode *node = group->first;
-    if (node != NULL) {
-        do {
-            temp = node->next;
-            free(node);
-            node = temp;
-        } while (node != group->first);
-    }
+    freeLinkedList(group->verticesList);
     freeMatrix(group->bHatSubMatrix);
     group->edgeSubMatrix->free(group->edgeSubMatrix);
 }
 
-VertexNode *addVertexToGroup(VerticesGroup *group, int index) {
-    VertexNode *node = malloc(sizeof(VertexNode));
-    node->index = index;
-    if (group->size != 0) {
-        node->next = group->first;
-        node->prev = group->first->prev;
-        group->first->prev->next = node;
-        group->first->prev = node;
-    } else {
-        group->first = node;
-        node->prev = node;
-        node->next = node;
-    }
-    group->size++;
-    return node;
+void addVertexToGroup(VerticesGroup *group, int index) {
+    insertItem(group->verticesList, NULL, index);
 }
 
 /**
@@ -53,6 +32,10 @@ void addSequence(VerticesGroup *group, int *sequence, int length){
         addVertexToGroup(group, sequence[i]);
 }
 
+int groupSize(VerticesGroup *group){
+    return group->verticesList->length;
+}
+
 /**
  * Create sub matrix
  * @param A edges matrix
@@ -61,23 +44,23 @@ void addSequence(VerticesGroup *group, int *sequence, int length){
  * @return Fills group->bSubMatrix and group->bHatSubMatrix.
  */
 void calculateSubMatrix(Matrix *A, int M, VerticesGroup *group) {
-    VertexNode *node;
+    LinkedListNode *node, *first;
     double *row, expectedEdges;
-    int i = 0, j;
-    if (group->size != 0) {
-        group->edgeSubMatrix = spmat_allocate_list(group->size);
-        group->bSubMatrix = createMatrix(group->size);
-        group->bHatSubMatrix = createMatrix(group->size);
-        group->verticesArr = malloc(sizeof(int) * group->size);
-        row = malloc(sizeof(double) * group->size);
-        node = group->first;
+    int i = 0, j, size = groupSize(group);
+    if (size != 0) {
+        group->edgeSubMatrix = spmat_allocate_list(size);
+        group->bSubMatrix = createMatrix(size);
+        group->bHatSubMatrix = createMatrix(size);
+        group->verticesArr = malloc(sizeof(int) * size);
+        row = malloc(sizeof(double) * size);
+        node = first = group->verticesList->first;
         do {
             group->verticesArr[i] = node->index;
             i++;
             node = node->next;
-        } while (node != group->first);
-        for (i = 0; i < group->size; i++) {
-            for (j = 0; j < group->size; j++) {
+        } while (node != first);
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
                 row[j] = readVal(A, group->verticesArr[i], group->verticesArr[j]);
                 /* For each vertex v: A[v][0] + ... + A[v][n-1] = deg(v) */
                 expectedEdges = A->rowSums[group->verticesArr[i]] * A->rowSums[group->verticesArr[j]] / M;
--- VerticesGroup.h
+++ VerticesGroup.h
@@ -3,30 +3,26 @@
 
 #include "matrix.h"
 #include "spmat.h"
-
-typedef struct vertexNode {
-    int index;
-    struct vertexNode *next;
-    struct vertexNode *prev;
-} VertexNode;
+#include "LinkedList.h"
 
 typedef struct verticesGroup {
-    VertexNode *first;
-    int size;
     int *verticesArr;
     spmat *edgeSubMatrix;
     Matrix *bSubMatrix;
     Matrix *bHatSubMatrix;
+    struct _LinkedList *verticesList;
 } VerticesGroup;
 
 VerticesGroup *createVerticesGroup();
 
 void freeVerticesGroup(VerticesGroup *group);
 
-VertexNode *addVertexToGroup(VerticesGroup *group, int index);
+void addVertexToGroup(VerticesGroup *group, int index);
 
 void addSequence(VerticesGroup *group, int *sequence, int length);
 
 void calculateSubMatrix(Matrix *A, int M, VerticesGroup *group);
+
+int groupSize(VerticesGroup *group);
 
 #endif
